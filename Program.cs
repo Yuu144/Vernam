e87b2c97489b4f9892f8956b69a8bd61d51c2986
Yuu144 <97678250+Yuu144@users.noreply.github.com>
2022-01-20 12:56:15 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    private static void Main()
+    {
+        string key = "test";
+        string message = File.ReadAllText("test.txt");
+        string encrypted = null;
+        string decrypted = null;
+
+        for(int i = 0; i < message.Length; i++)
+        {
+            int j = i;
+
+            if (j >= key.Length)
+                j = 0;
+
+            encrypted += (char)((int)message[i] + (int)key[j]);
+        }
+
+        for (int i = 0; i < message.Length; i++)
+        {
+            int j = i;
+
+            if (j >= key.Length)
+                j = 0;
+
+            decrypted += (char)((int)encrypted[i] - (int)key[j]);
+        }
+
+        Console.WriteLine(encrypted);
+        Console.WriteLine(decrypted);
+        Console.ReadLine();
+    }
+}
